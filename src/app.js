fac0353f65a095259ffcaa3995b3f1af0aa75ed3
Akilesh-B-Naik <akilesh.n@tracxn.com>
2021-08-19 11:18:23 +0530

    moified app.js file

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-// stateless functional component
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css'
@@ -15,55 +14,66 @@ class Table extends React.Component {
       users: [],
       total: undefined,
       pageNo: 1,
-     
-
+      loading: false
     }
 
-
   componentDidMount() {
+    console.log("mount executed");
     this.getPosts();
     window.addEventListener("scroll", (event) => {
-
-      if ((document.body.scrollHeight) < (window.innerHeight + window.scrollY)) {
-       
+      console.log("event listener executed");
+      console.log(document.body.scrollHeight);
+      console.log(window.innerHeight + window.scrollY);
+      if (document.body.scrollHeight - (window.innerHeight + window.scrollY) <= 30) {
         this.getPosts();
       }
-    
     });
   }
 
   getPosts = async () => {
-    const { pageNo, total,users } = this.state;
-    
-   if(total===undefined || users.length<total )
-   {
-    const val = await fetch('https://jsonplaceholder.typicode.com/posts?_page=' + pageNo)
-    const response = await val.json();
-    const totalCount = val.headers.get("x-total-count");
+    console.log("get post executed")
+    const { pageNo, total, users, loading } = this.state;
+    if (loading) {
+      return
+    }
+    this.setState({ loading: true })
+    if (total === undefined || users.length < total) {
+      const val = await fetch('https://jsonplaceholder.typicode.com/posts?_page=' + pageNo)
+      const response = await val.json();
+      const totalCount = val.headers.get("x-total-count");
 
       this.setState({
         total: totalCount,
         pageNo: pageNo + 1,
-        users: users.concat(response)
+        users: users.concat(response),
+        loading: false
       })
-   }
+    }
   }
-
   render() {
 
     const { users } = this.state;
     //const val=this.state;
     return (
       <div>
+        <table id="users" border='1'>
+          <tbody>
+            <tr>
+              <td>ID</td>
+              <td>Title</td>
+              <td>BODY</td>
+            </tr>
 
-        <ul>
-          {users.map(user => (
-            <li key={user.id}>
-              {user.id} {user.title} {user.body}
-            </li>
-          ))}
-        </ul>
+            {users.map(user => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.title}</td>
+                <td>{user.body}</td>
+              </tr>
+            ))}
 
+          </tbody>
+        </table>
 
       </div>
     )
